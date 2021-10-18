ba16bf759b2089110ae90574a13d5194ecff1f72
chris.am <c.aranguizm@outlook.com>
2021-10-17 22:56:07 -0300

    create product method added

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,2 +1,80 @@
-const {response, reques} = require('express');
+const { response, request } = require('express');
+const Product  = require('../models/products.model');
 
+const createProduct = async (req = request, res = response) => {
+    
+    try {
+        const product = {
+            codebar: req.body.codebar,
+            name: req.body.name
+        }
+        const prodInDb = await Product.create(product);
+        console.log('prodInDb', prodInDb )
+        res.json({
+            ok: true,
+            msg: 'doctor added',
+            prodInDb
+        });
+    } catch (error) {
+        console.log('Error --->', error);
+        res.status(500).json({
+            ok:false,
+            msg: 'Internal server error. Check logs'
+        })
+    }
+}
+const getProductById = async (req = request, res = response) => {
+    try {
+        
+    } catch (error) {
+        console.log('Error --->', error);
+        res.status(500).json({
+            ok:false,
+            msg: 'Internal server error. Check logs'
+        })
+    }
+}
+
+const getProducts = async (req = request, res = response) => {
+    try {
+        
+    } catch (error) {
+        console.log('Error --->', error);
+        res.status(500).json({
+            ok:false,
+            msg: 'Internal server error. Check logs'
+        })
+    }
+}
+
+const updateProducts = async (req = request, res = response) => {
+    try {
+        
+    } catch (error) {
+        console.log('Error --->', error);
+        res.status(500).json({
+            ok:false,
+            msg: 'Internal server error. Check logs'
+        })
+    }
+}
+
+const deleteProduct = async (req = request, res = response) => {
+    try {
+        
+    } catch (error) {
+        console.log('Error --->', error);
+        res.status(500).json({
+            ok:false,
+            msg: 'Internal server error. Check logs'
+        })
+    }
+}
+
+module.exports = {
+    createProduct,
+    getProductById,
+    getProducts,
+    updateProducts,
+    deleteProduct
+}
